646e482b47937bc027fdfd74ee3ab9fad87468f5
Ulku Arikanoglu <arikanogluulku@gmail.com>
2021-11-20 17:24:58 +0300

    menu title components added

--- src/components/MenuTitle.jsx
+++ src/components/MenuTitle.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function MenuTitle(props) {
+function MenuTitle({ info }) {
   return (
     <div className="menu__header">
-      <h1 className="menu__header-title">{props.info}</h1>
+      <h1 className="menu__header-title">{info}</h1>
     </div>
   );
 }
 
-MenuTitle.propTypes = {
-  info: PropTypes.string.isRequired,
-};
 export default MenuTitle;
